b76cd0e3727a300bd01064145a82c9a1bf6b3611
Lanh Huynh <lanhdn90@gmail.com>
2021-02-24 20:36:48 +0700

    create Header

--- src/component/TodoApp.js
+++ src/component/TodoApp.js
@@ -1,18 +1,15 @@
-// Bước 1:
+
 import React from "react";
+import Header from "../component/layout/Header";
+
 class TodoApp extends React.Component{
-    // Chúng ta định nghĩa một React component bằng cách tạo một class 
-    // và kế thừa (extend) từ Component class của thư viện React
+
     render(){
-        // chúng ta phải sử dụng hàm render() trả về một JSX element 
-        // để render giao diện lên màn hình
+
         return(
-            // Bạn không thể trả về nhiều hơn một JSX element ngang hàng nhau. 
-            // Trả về nhiều element nhưng chúng phải lồng nha, đảm bảo chỉ có duy nhất một element gốc. 
-            // Có một giải pháp là bạn cho hết vào một thẻ <div>.
+
             <div>
-                <h1>Hello from Create React App</h1>
-                <p>I am in React Component!</p>
+                <Header/>
             </div>
         );
     }
